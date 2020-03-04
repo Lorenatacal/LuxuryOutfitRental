b47060cd65aa500499a00357565298abaa55fe0f
Lorena <lorena.tacal@gmail.com>
2020-03-04 16:22:12 +0000

    Add user model

---
+++ models/Users/User.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const UserSchema = new mongoose.Schema({
+    userName: String,
+    email: {
+        type: String,
+        required: true,
+        // validate: {
+        //     validator: function() {
+        //         return validator.default.isEmail(this.email)
+        //     }
+        // }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 20
+    }
+})
+
+// UserSchema.methods.generateAuthToken = function () {
+//     return jwt.sign(this.id, process.env.JWT_SECRET)
+// }
+
+UserSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(this.password, salt)
+    this.password = hashedPassword
+})
+
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports = UserModel
--- models/Users/User.model.test.js
+++ models/Users/User.model.test.js
@@ -3,6 +3,25 @@ const bcrypt = require('bcrypt');
 const User = require('./User.model');
 
 beforeAll(async () => {
-    const url = 'mongodb://127.0.0.1/Store'
+    const url = 'mongodb://127.0.0.1/LuxuryOutfitRentalShop'
     await mongoose.connect(url, { useNewUrlParser: true })
+})
+
+describe('Password storage', () => {
+    let user
+
+    beforeAll(async (done) => {
+        user = await User.create({ userName: 'Tudor', email: 'tudortacal@domanin.com', password: 'randomPass' })
+        done()
+    })
+
+    test('Does not store the plain text password', () => {
+        expect(user.password).not.toBeUndefined()
+        expect(user.password).not.toBe('randomPass')
+    })
+    test('specifically stores an encrypted version of the plaintext password', async (done) => {
+        const isMatched = await bcrypt.compare('randomPass', user.password)
+        expect(isMatched).toBeTruthy()
+        done()
+    })
 })
